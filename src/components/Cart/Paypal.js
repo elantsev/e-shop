80360f21b6208f23937ce117132a8fe4371f15ab
Павел <elantsev.pa@gmail.com>
2019-10-11 18:46:56 +0300

    save PAYPAL_ID in .env

--- src/components/Cart/Paypal.js
+++ src/components/Cart/Paypal.js
@@ -27,11 +27,11 @@ export default class MyApp extends React.Component {
 
     let env = "sandbox"; // you can set here to 'production' for production
     let currency = "RUB"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    // let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
-
+    // sandbox:process.env.
     const client = {
-      sandbox: process.env.PAYPAL_ID,
+      sandbox: process.env.REACT_APP_PAYPAL_ID,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
@@ -46,7 +46,7 @@ export default class MyApp extends React.Component {
         env={env}
         client={client}
         currency={currency}
-        total={this.props.totalAmount}
+        total={this.props.total}
         onError={onError}
         onSuccess={onSuccess}
         onCancel={onCancel}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import { ProductProvider } from "./context";
+// require("dotenv").config();
 
 ReactDOM.render(
   <ProductProvider>
