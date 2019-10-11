1629f66dfbd1bab224fefacea462603227f38b88
Павел <elantsev.pa@gmail.com>
2019-10-11 15:38:41 +0300

    Default component

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Default = () => {
+const Default = props => {
   return (
-    <div>
-      <h3>Page not found</h3>
+    <div className="container">
+      <h1>404</h1>
+      <h1>error</h1> 
+      <h2>page not found</h2>
+      <h3>
+        requested URL{" "}
+        <span className="text-danger">{props.location.pathname}</span> was not
+        found
+      </h3>
     </div>
   );
 };
